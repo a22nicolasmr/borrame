ba0bf551df14c8cce38793b2fb00c0c3977ef3a6
a22nicolasmr@iessanclemente.net <>
2023-03-13 17:00:14 +0100

    Arranxados todos os erros

--- ProxectoErros/src/main/java/com/mycompany/proxectoerros/ProxectoErros.java
+++ ProxectoErros/src/main/java/com/mycompany/proxectoerros/ProxectoErros.java
@@ -8,13 +8,14 @@ public class ProxectoErros {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        int num=1246;
+        int num=789;
         int resto;
          int numInvertido = 0;
         
         while(num!=0){
-            resto=num%2;
+            resto=num%10;
             numInvertido=(numInvertido)*10+resto;
+            num/=10;
         }
         System.out.println("O numero invertido e "+numInvertido);
     }
